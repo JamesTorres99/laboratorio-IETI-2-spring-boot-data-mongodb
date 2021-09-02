fd68632d31fabffbb6caf3ff83501ae7d96e6475
JamesTorres99 <69390068+JamesTorres99@users.noreply.github.com>
2021-09-02 05:05:52 -0500

    add a controller

--- src/main/java/org/ada/school/service/UserServiceHashMap.java
+++ src/main/java/org/ada/school/service/UserServiceHashMap.java
@@ -2,13 +2,14 @@ package org.ada.school.service;
 
 import org.ada.school.dto.UserDto;
 import org.ada.school.model.User;
+import org.ada.school.repository.UserDocument;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-@Service
+
 public class UserServiceHashMap
     implements UserService
 {
@@ -17,26 +18,21 @@ public class UserServiceHashMap
 
 
     @Override
-    public User create( User user )
-    {
-        usersMap.put( user.getId(), user );
-        return user;
-    }
-
-    @Override
-    public User findById( String id )
-    {
-        if ( usersMap.containsKey( id ) )
-        {
-            return usersMap.get( id );
-        }
+    public UserDocument create(UserDocument user) {
         return null;
     }
 
     @Override
-    public List<User> all()
+    public UserDocument findById( String id )
     {
-        return new ArrayList<>( usersMap.values() );
+
+        return null;
+    }
+
+    @Override
+    public List<UserDocument> all()
+    {
+        return null;
     }
 
     @Override
@@ -46,18 +42,10 @@ public class UserServiceHashMap
     }
 
     @Override
-    public User update( UserDto userDto, String id )
+    public UserDocument update( UserDto userDto, String id )
     {
-        if ( usersMap.containsKey( id ) )
-        {
-            User user = usersMap.get( id );
-            user.update( userDto );
-            return user;
-        }
-        else
-        {
+
             return null;
-        }
     }
 
 
